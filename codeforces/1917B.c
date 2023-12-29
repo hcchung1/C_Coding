ec0f78396d649e5dc305f445fc5c9ab1ca7b8de1
鍾承翰 <131962610+hcchung1@users.noreply.github.com>
2023-12-29 15:54:08 +0000

    modified:   codeforces/1917B.c

--- codeforces/1917B.c
+++ codeforces/1917B.c
@@ -2,25 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void count_distinct_strings(char* s) {
+void count_distinct_strings(char* s, char** database) {
     // Find the number of different substrings of s, by deleting the first or the second character of s.
     int count = 0;
     char* t= (char*)malloc(strlen(s));
-    char** database = (char**)malloc(strlen(s) * sizeof(char*));
-    for (int i = 0; i < strlen(s); i++) {
-        database[i] = (char*)malloc(strlen(s) * sizeof(char));
-    }
-    for (int i = 0; i < strlen(s); i++) {
-        for (int j = 0; j < strlen(s); j++) {
-            if (i != j) {
-                strcpy(t, s);
-                t[j] = '\0';
-                strcat(t, s + j + 1);
-                strcpy(database[count], t);
-                count++;
-            }
-        }
-    }
+    
+    
     
     // Print the number of distinct substrings.
     printf("%d\n", count);
@@ -39,12 +26,18 @@ int main() {
 
         // Read the string.
         char* s = (char*)malloc((n + 1) * sizeof(char));
+        char** database = (char**)malloc(strlen(s) * sizeof(char*));
+        for (int i = 0; i < strlen(s); i++) {
+            database[i] = (char*)malloc(strlen(s) * sizeof(char));
+        }
         scanf("%s", s);
         s[n] = '\0';
         
 
         // Count the number of distinct strings.
         count_distinct_strings(s);
+        free(s);
+        free(database);
     }
 
     return 0;
