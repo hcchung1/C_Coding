0ece52426811d8a434dbd2ad557fd3ec8aa05245
鍾承翰 <131962610+hcchung1@users.noreply.github.com>
2023-12-29 16:34:31 +0000

    modified:   codeforces/1917D 	modified:   codeforces/1917D.c

--- codeforces/1917D.c
+++ codeforces/1917D.c
@@ -3,43 +3,45 @@
 
 int main(){
 
-    int n;
-    scanf("%d", &n);
+    long long int n;
+    scanf("%lld", &n);
 
     while(n--){
-
         // input a, b
-        int a, b;
-        scanf("%d %d", &a, &b);
+        long long int a, b;
+        scanf("%lld %lld", &a, &b);
 
         //construct alpha, beta
-        int* alpha = (int*)malloc(a*sizeof(int));
-        int* beta = (int*)malloc(b*sizeof(int));
-        for(int i=0; i<a; i++){
-            scanf("%d", &alpha[i]);
+        long long int* alpha = (long long int*)malloc(a*sizeof(long long int));
+        long long int* beta = (long long int*)malloc(b*sizeof(long long int));
+        for(long long int i=0; i<a; i++){
+            scanf("%lld", &alpha[i]);
         }
-        for(int i=0; i<b; i++){
-            scanf("%d", &beta[i]);
+        for(long long int i=0; i<b; i++){
+            scanf("%lld", &beta[i]);
         }
 
         // calculate the array
-        int* arr = (int*)malloc((a*b)*sizeof(int));
-        for(int i=0; i<a; i++){
-            for(int j=0; j<b; j++){
+        long long int* arr = (long long int*)malloc((a*b)*sizeof(long long int));
+        for(long long int i=0; i<a; i++){
+            for(long long int j=0; j<b; j++){
                 arr[i*b+j] = alpha[i]*pow(2, beta[j]);
             }
         }
 
-        int count = 0;
+        long long int count = 0;
 
-        for(int i = 0; i<a*b; i++){
-            for(int j = i; j < a*b; j++){
+        for(long long int i = 0; i<a*b; i++){
+            for(long long int j = i; j < a*b; j++){
                 if(arr[i] > arr[j]){
                     count++;
                 }
             }
         }
-        printf("%d\n", count);
+        printf("%lld\n", count%998244353);
+        free(alpha);
+        free(beta);
+        free(arr);
     }
 
 }
